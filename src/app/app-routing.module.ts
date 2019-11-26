532b4c96f16a4b641b4eff67bea86849ce1f9f11
Luismoteando <LuisMiguel.Ortiz.Rozalen@alumnos.upm.es>
2019-11-26 19:02:39 +0100

    Components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ServicesComponent } from './services/services.component';
+import { InstallationsComponent } from './installations/installations.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'servicios', component: ServicesComponent },
+  { path: 'instalaciones', component: InstallationsComponent},
+  { path: 'reservas', component: ReservationsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signin', component: SigninComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { InstallationsComponent } from './installations/installations.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    InstallationsComponent,
+    LoginComponent,
+    SigninComponent,
+    ReservationsComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
